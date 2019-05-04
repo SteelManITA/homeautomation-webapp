16274ebda702fb54192b34649dc57ce607fc9c00
SteelMan <concetto.pollicina@gmail.com>
2019-05-04 16:07:45 +0200

    feat(home): add home page logic

--- src/themes/default/views/home/home.ts
+++ src/themes/default/views/home/home.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
+import { HomeAutomationRequestService } from '@app/services';
+import { Room } from '@app/models';
 
 @Component({
   templateUrl: 'home.html'
 })
 export class HomePage {
+  rooms: Room[] = [];
+  devicesCount: number = 0;
+
+  constructor(
+    private request: HomeAutomationRequestService,
+  ) {
+    this.request.getRooms().
+      then((rooms) => {
+        console.log('riuscito', rooms);
+        this.rooms = rooms;
+        for (const room of this.rooms) {
+          this.devicesCount += room.devices.length;
+        }
+      }).
+      catch((err) => {
+        console.log('fallito', err);
+      });
+  }
 }
