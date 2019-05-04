5e97c31e603f4618c7febd49365df4e74876b874
SteelMan <concetto.pollicina@gmail.com>
2019-05-04 15:50:11 +0200

    feat(parameters): add parameters service

---
+++ src/lib/angular/core/services/parameters.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  ActivatedRoute,
+  NavigationEnd,
+} from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ParametersService {
+  private params: any;
+  private ready: boolean = false;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe((event) => {
+        let r: any = this.route;
+        // we need to use fir st, or we will end up having
+        // an increasing number of subscriptions after each route change.
+        while (r.firstChild) {
+          r = r.firstChild;
+        }
+
+        this.params = {};
+
+        r.params.subscribe(params => {
+          this.params = Object.assign(this.params, params);
+        });
+
+        r.queryParams.subscribe(queryParams => {
+          this.params = Object.assign(this.params, queryParams);
+        });
+      });
+  }
+
+  getParams(): any {
+    return this.params;
+  }
+}
--- src/lib/angular/services.ts
+++ src/lib/angular/services.ts
@@ -1,4 +1,5 @@
 export * from './core/services/api.service';
 export * from './core/services/i18n.service';
 export * from './core/services/local-storage.service';
+export * from './core/services/parameters.service';
 export * from './core/services/session-storage.service';
