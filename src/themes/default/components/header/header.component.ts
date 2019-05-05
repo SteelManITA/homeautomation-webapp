7669d963faa8d2f8d59361b6b900c51260d8a9e8
SteelMan <concetto.pollicina@gmail.com>
2019-05-05 13:06:25 +0200

    fix(header): set as unselected not selected menu fields

--- src/themes/default/components/header/header.component.ts
+++ src/themes/default/components/header/header.component.ts
@@ -45,7 +45,8 @@ export class HeaderComponent {
             )
           ) {
             menu[index].selected = true;
-            return;
+          } else {
+            menu[index].selected = false;
           }
         });
       });
